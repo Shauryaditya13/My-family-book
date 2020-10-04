3c46acce9df88b9688393f39e8fbce9fe7d2e79e
Shauryaditya13 <71700044+Shauryaditya13@users.noreply.github.com>
2020-10-04 10:53:43 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -5,10 +5,10 @@ var reason = [
     "Shauryaditya"
   ];
   var pics = [
-    "blob:https://web.whatsapp.com/68a1fe88-e9a9-40f9-abb0-fb4838da0dc5",
-    "blob:https://web.whatsapp.com/9a2c1f67-e80e-427e-8282-ff0de1f692b2",
-    "blob:https://web.whatsapp.com/04ab24eb-fd39-42d3-930a-ad4b8d737428",
-    "blob:https://web.whatsapp.com/dc3facaa-e431-4998-8408-2246470513da"
+    "Kiran Kumar.jpg",
+    "Swapna.jpg",
+    "aishwarya.jpg",
+    "Shauryaditya.jpg"
   ];
   var arraynumber = 0;
   function nextslide() {
